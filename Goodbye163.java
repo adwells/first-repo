6eac773e7124ff13f31b9e3f2c2a0c201338dda0
adwells <45216851+adwells@users.noreply.github.com>
2018-11-20 19:27:48 -0500

    Create Goodbye163.java

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/**
+ * Prints Goodbye Comp 163 to be put into GITHUB.
+ * @author adwells@aggies.ncat.edu
+ */
+public class Goodbye163 {
+  public static void main(String[] args) {
+    System.out.println("Goodbye COMP 163.");
+  }
+}
